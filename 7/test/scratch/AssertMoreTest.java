b22496c4a8eb68ce7d1b656ca8e9b968713a7e44
古川広一 <ex.kouichi1224@gmail.com>
2021-01-01 21:56:46 +0900

    Add: 4.6 @Beforeと@Afterに関する補足(初期化と後処理)

---
+++ 7/test/scratch/AssertMoreTest.java
@@ -0,0 +1,36 @@
+package scratch;
+
+import org.junit.*;
+
+public class AssertMoreTest {
+
+    @BeforeClass
+    public static void initializeSomethingReallyExpensive() {
+        // ...
+    }
+
+    @AfterClass
+    public static void cleanUpSomethingReallyExpensive() {
+        // ...
+    }
+
+    @Before
+    public void createAccount() {
+        // ...
+    }
+
+    @After
+    public void closeConnections() {
+        // ...
+    }
+
+    @Test
+    public void depositIncreasesBalance() {
+        // ...
+    }
+
+    @Test
+    public void hasPositiveBalance() {
+        // ...
+    }
+}
